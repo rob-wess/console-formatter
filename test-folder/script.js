b1ed7d904b54bfa0cd1cbc035a08e52c8154da27
rob-wess <robwessgit@gmail.com>
2021-09-06 10:37:30 -0400

    test script

--- test-folder/script.js
+++ test-folder/script.js
@@ -1,12 +1,25 @@
 //const consoleFormatter = require('console-formatter');
+
+const systemdStatus = require('systemd-status');
 const printTitle = require('console-formatter').printTitle;
 const printMsg = require('console-formatter').printMsg;
 
+printTitle('Test Systemd Service Status');
+
+const currentStatus = systemdStatus(['httpd', 'firewalld' ]);
+
+for (i=0; i<currentStatus.length; i++) {
+	serviceName = currentStatus[i].name;
+	serviceState = currentStatus[i].state;
 
 
-printTitle('Show Demo of Options')
-printMsg('Successful command', 0)
-printMsg('Failed on exit', 1)
-printMsg('There is a warning somewhere', 2)
-printMsg('Show an infomrational', 3)
-console.log('\n')
+	if (serviceState == 'running') {
+		var rc = 0;
+	} else if (serviceState = 'dead') {
+		var rc = 2;
+	}
+
+	printMsg("Check " + serviceName + " is running", rc );
+}
+
+console.log('\n');
